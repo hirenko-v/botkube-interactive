8909efc6d499dadef5807c804e5739b8dff82aeb
hirenko-v <132065511+hirenko-v@users.noreply.github.com>
2024-09-23 00:19:39 +0300

    test

--- cmd/msg/main.go
+++ cmd/msg/main.go
@@ -221,7 +221,7 @@ func showBothSelects(state map[string]string) executor.ExecuteOutput {
 				Text:        "test",
 				Placeholder: "test",
 				DispatchedAction: api.DispatchInputActionOnCharacter,
-			}
+			},
 		},
 	}
 
